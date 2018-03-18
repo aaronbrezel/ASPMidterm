998da0e69a20a7c36c07f7004488693893bc2784
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-03-18 13:58:25 -0500

    Testing out a concept for the Rasch class
    added a setValidity method. Only requirement I can think of right now besides the type of the inputs are that "a" and "yJ" need to have the same length

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -11,3 +11,35 @@ document(current.code)
 check(current.code)
 
 
+##test math
+
+setClass(Class="Rasch",  #setClass method. creates Student class with three parameters: name, a and yJ
+         representation = representation(
+           name = "character",
+           a = "numeric",
+           yJ = "numeric"
+         ),
+         prototype = prototype(
+           name = c(),
+           a = c(),
+           yJ = c()
+         )
+)
+
+setMethod("initialize", "Rasch", #initialize method
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+setValidity("Rasch", function(object){ #Validity Function
+  if(!identical(length(object@a), length(object@yJ))){ #a and yJ need to have an identical lengths. That's because their indecies must match up in order to pair the right question difficulty (a) with the corresponding question answer (yJ)
+    return("Yo, vectors a and yJ must have identical lengths")
+  }
+})
+
+a <- c(1,2,3,4,5,6,7,8,9,10)
+yJ <- c(1,0,1,1,0,1,1,1,0,1)
+Brian <- new("Rasch", name = "Brian", a = a, yJ = yJ)
+Brian
